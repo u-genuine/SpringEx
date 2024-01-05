f49d49d906e8b2b77c63df0f8a5b6ed1e78d560e
u-genuine <u_genuine@naver.com>
2024-01-05 15:25:37 +0900

    SampleDAO 주입하기

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,4 +1,12 @@
 package org.zerock.springex.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
 public class SampleService {
+
+    @Autowired
+    private SampleDAO sampleDAO;
+
 }
