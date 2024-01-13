9c1901a591790caee767ddf2e496a4ca37e11f8b
u-genuine <u_genuine@naver.com>
2024-01-13 15:51:20 +0900

    @Valid 서버사이드 검증

--- src/main/java/org/zerock/springex/controller/TodoController.java
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,11 +2,14 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/todo") // @RequestMapping(value = "/todo")와 같은 의미. 해당 경로의 요청을 지정하기 위해 사용
@@ -35,9 +38,15 @@ public class TodoController {
     }
 
     @PostMapping("/register")
-    public String registerPOST(TodoDTO todoDTO, RedirectAttributes redirectAttributes){
+    public String registerPOST(@Valid TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
 
         log.info("POST todo register......");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/todo/register";
+        }
 
         log.info(todoDTO);
 
--- src/main/webapp/WEB-INF/views/todo/register.jsp
+++ src/main/webapp/WEB-INF/views/todo/register.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!doctype html>
 <html lang="en">
@@ -68,6 +69,17 @@
                                 </div>
                             </div>
                         </form>
+                        <script>
+                            const serverValidResult = {}
+
+                            <c:forEach items="${errors}" var="error">
+
+                            serverValidResult['${error.getField()}'] = '${error.defaultMessage}'
+
+                            </c:forEach>
+
+                            console.log(serverValidResult)
+                        </script>
                     </div>
                 </div>
             </div>
